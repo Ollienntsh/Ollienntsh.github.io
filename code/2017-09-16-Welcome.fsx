9c80bbe85ee873cdeae9eaf3b7365b4affee6b0f
Ante Machiedo <amachiedo@rhapsody.com>
2017-09-17 22:19:00 +0200

    Initial commit.

---
+++ code/2017-09-16-Welcome.fsx
@@ -0,0 +1,20 @@
+(*** raw ***)
+---
+layout: page
+title: Welcome
+---
+
+(**
+
+My first post
+=============
+
+Please check the following example in F#
+*)
+
+let example = 
+    "awesome"
+    |> Seq.iter (fun x -> printfn "%c" x)
+
+[lang=csharp]
+Console.WriteLine("Welcome to my personal blog!");
